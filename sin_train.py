4ef3af48b61b6fb301a727291fc75fa689ce599f
kaz-ojisan <YanyanyanKaz183@gmail.com>
2020-03-25 03:48:22 +0900

    create　.gitignore

--- sin_train.py
+++ sin_train.py
@@ -3,14 +3,13 @@
 import numpy as np # 配列
 import time # 時間
 from matplotlib import pyplot as plt # グラフ
-import os # フォルダを作成可能（意外と便利）
+import os # フォルダ作成のため
 
-# chainer
-from chainer import Variable, optimizers, serializers
-from chainer import Link, Chain, ChainList
-import chainer.functions as F
-import chainer.links as L
-
+# pytorch
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
 
 # y=sin(x)のデータセットをN個分作成
 def get_data(N, Nte):
@@ -25,24 +24,24 @@ def get_data(N, Nte):
 
     return x_train, t_train, x_test, t_test
 
-class SIN_NN(Chain):
+# Neural Network
+class SIN_NN(nn.Module):
     def __init__(self, h_units, act):
         super(SIN_NN, self).__init__()
-        with self.init_scope():
-            self.l1=L.Linear(1, h_units[0])
-            self.l2=L.Linear(h_units[0], h_units[1])
-            self.l3=L.Linear(h_units[1], 1)
+        self.l1=nn.Linear(1, h_units[0])
+        self.l2=nn.Linear(h_units[0], h_units[1])
+        self.l3=nn.Linear(h_units[1], 1)
 
-            if act == "relu":
-                self.act = F.relu
-            elif act == "sig":
-                self.act = F.sigmoid
+        if act == "relu":
+            self.act = F.relu
+        elif act == "sig":
+            self.act = F.sigmoid
 
     def __call__(self, x, t):
-        x = Variable(x.astype(np.float32).reshape(x.shape[0],1))
-        t = Variable(t.astype(np.float32).reshape(t.shape[0],1))
-
-        return F.mean_squared_error(self.forward(x), t)
+        x = T.from_numpy(x.astype(np.float32).reshape(x.shape[0],1))
+        t = T.from_numpy(t.astype(np.float32).reshape(t.shape[0],1))
+        y = self.forward(x)
+        return y, t
 
     def forward(self, x):
         h = self.act(self.l1(x))
@@ -52,21 +51,24 @@ class SIN_NN(Chain):
         return h
 
     def predict(self, x):
-        x = Variable(x.astype(np.float32).reshape(x.shape[0],1))
+        x = T.from_numpy(x.astype(np.float32).reshape(x.shape[0],1))
         y = self.forward(x)
 
         return y.data
 
-# 学習（学習パラメータを設定しやすくするために関数化）
+
 def training(N, Nte, bs, n_epoch, h_units, act):
 
     # データセットの取得
     x_train, t_train, x_test, t_test = get_data(N, Nte)
+    x_test_torch = T.from_numpy(x_test.astype(np.float32).reshape(x_test.shape[0],1))
+    t_test_torch = T.from_numpy(t_test.astype(np.float32).reshape(t_test.shape[0],1))
 
     # モデルセットアップ
     model = SIN_NN(h_units, act)
-    optimizer = optimizers.Adam()
-    optimizer.setup(model)
+    optimizer = optim.Adam(model.parameters())
+    # optimizer.setup(model)
+    MSE = nn.MSELoss()
 
     # loss格納配列
     tr_loss = []
@@ -82,16 +84,18 @@ def training(N, Nte, bs, n_epoch, h_units, act):
 
     # 学習回数分のループ
     for epoch in range(1, n_epoch + 1):
+        model.train()
         perm = np.random.permutation(N)
         sum_loss = 0
         for i in range(0, N, bs):
             x_batch = x_train[perm[i:i + bs]]
             t_batch = t_train[perm[i:i + bs]]
 
-            model.cleargrads()
-            loss = model(x_batch,t_batch)
+            optimizer.zero_grad()
+            y_batch, t_batch = model(x_batch, t_batch)
+            loss = MSE(y_batch, t_batch)
             loss.backward()
-            optimizer.update()
+            optimizer.step()
             sum_loss += loss.data * bs
 
         # 学習誤差の平均を計算
@@ -99,7 +103,9 @@ def training(N, Nte, bs, n_epoch, h_units, act):
         tr_loss.append(ave_loss)
 
         # テスト誤差
-        loss = model(x_test,t_test)
+        model.eval()
+        y_test_torch = model.forward(x_test_torch)
+        loss = MSE(y_test_torch, t_test_torch)
         te_loss.append(loss.data)
 
         # 学習過程を出力
@@ -122,10 +128,10 @@ def training(N, Nte, bs, n_epoch, h_units, act):
 
         if epoch % 20 == 0:
             # epoch20ごとのテスト予測結果
-            plt.figure(figsize=(3, 2))
+            plt.figure(figsize=(4, 3))
             y_test = model.predict(x_test)
-            plt.plot(x_test, t_test)
-            plt.plot(x_test, y_test)
+            plt.plot(x_test, t_test, label = "target")
+            plt.plot(x_test, y_test, label = "predict")
             plt.legend()
             plt.grid(True)
             plt.xlim(0, 2 * np.pi)
@@ -155,38 +161,8 @@ def training(N, Nte, bs, n_epoch, h_units, act):
     plt.clf()
     plt.close()
 
-    # 最終のテスト予測結果
-    plt.figure(figsize=(3, 2))
-    y_test = model.predict(x_test)
-    plt.plot(x_test, t_test)
-    plt.plot(x_test, y_test)
-    plt.grid(True)
-    plt.xlim(0, 2 * np.pi)
-    plt.ylim(-1.2, 1.2)
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.savefig("Results/{}/Pred/ep{}.png".format(act,epoch))
-    plt.clf()
-    plt.close()
-
-    # 最終のテスト予測結果
-    plt.figure(figsize=(4, 3))
-    y_test = model.predict(x_test)
-    plt.plot(x_test, t_test, label = "answer")
-    plt.plot(x_test, y_test, label = "prediction")
-    plt.legend()
-    plt.grid(True)
-    plt.xlim(0, 2 * np.pi)
-    plt.ylim(-1.2, 1.2)
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.savefig("Results/{}/Pred/ep{}_big.png".format(act,epoch))
-    plt.clf()
-    plt.close()
-
     # 学習済みモデルの保存
-    serializers.save_npz("Results/{}/Model.model".format(act),model)
-
+    T.save(model, "Results/model.pt")
 
 if __name__ == "__main__":
 
@@ -194,7 +170,7 @@ if __name__ == "__main__":
     N = 1000             # 学習データ
     Nte = 200           # テストデータ数
     bs = 10             # バッチサイズ
-    n_epoch = 10#00       # 学習回数
+    n_epoch = 200       # 学習回数
     h_units = [10, 10]  # ユニット数 [中間層１　中間層２]
     act = "relu"        # 活性化関数(ReLU関数にしたい場合は、"relu")
 
